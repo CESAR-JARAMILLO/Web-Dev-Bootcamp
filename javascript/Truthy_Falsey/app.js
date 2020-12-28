4cdbb7275953b0022ab1c7a2d37702cea402c354
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-12-28 14:13:38 -0800

    truthy and falsy

---
+++ javascript/Truthy_Falsey/app.js
@@ -0,0 +1,31 @@
+// const userInput = prompt("Enter something")
+
+// if (userInput) {
+//     console.log("Truthy!")
+// } else {
+//     console.log("Falsey!")
+// }
+
+// if (0) {
+//     console.log("Truthy!")
+// } else {
+//     console.log("Falsey!")
+// }
+
+// if (NaN) {
+//     console.log("Truthy!")
+// } else {
+//     console.log("Falsey!")
+// }
+
+// if (null) {
+//     console.log("Truthy!")
+// } else {
+//     console.log("Falsey!")
+// }
+
+if (undefined) {
+    console.log("Truthy!")
+} else {
+    console.log("Falsey!")
+}
