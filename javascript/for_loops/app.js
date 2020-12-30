b07e7ecacd6cafe3de080bc48fab5d89dc5f72da
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-12-30 10:47:17 -0800

    looping over arrays basics

--- javascript/for_loops/app.js
+++ javascript/for_loops/app.js
@@ -18,6 +18,20 @@
 // }
 
 
-for (let i = 25; i >=0; i -= 5) {
-    console.log(i)
+// for (let i = 25; i >=0; i -= 5) {
+//     console.log(i)
+// }
+
+
+
+// Looping Over Arrays
+
+const animals = ['horse', 'cat', 'dog', 'zebra', 'hippo']
+
+// for (let i = 0; i < animals.length; i++) {
+//     console.log(i, animals[i])
+// }
+
+for (let i = animals.length - 1; i >= 0; i--) {
+    console.log(i, animals[i])
 }
