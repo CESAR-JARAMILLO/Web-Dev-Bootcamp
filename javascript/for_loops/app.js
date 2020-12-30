1c348fed08a8fccbbf8eeb2387fb005c8e9ec829
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-12-30 13:37:24 -0800

    nested loops basics

--- javascript/for_loops/app.js
+++ javascript/for_loops/app.js
@@ -26,12 +26,44 @@
 
 // Looping Over Arrays
 
-const animals = ['horse', 'cat', 'dog', 'zebra', 'hippo']
+// const animals = ['horse', 'cat', 'dog', 'zebra', 'hippo']
 
-// for (let i = 0; i < animals.length; i++) {
+// // for (let i = 0; i < animals.length; i++) {
+// //     console.log(i, animals[i])
+// // }
+
+// for (let i = animals.length - 1; i >= 0; i--) {
 //     console.log(i, animals[i])
 // }
 
-for (let i = animals.length - 1; i >= 0; i--) {
-    console.log(i, animals[i])
+
+// Nested Loops
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`i is: ${i}`)
+//     for (let j = 1; j <= 4; j++) {
+//         console.log(`    j is: ${j}`)
+//     }
+// }
+
+
+const seatingChart = [
+    ['Kris', 'Bob', 'Jill'],
+    ['Bill', 'Jun', 'Sam', 'Tom'],
+    ['Kim', 'Jan', 'Krill', 'Sup']
+]
+
+// for(let i = 0; i < seatingChart.length; i++) {
+//     for (let j = 0; j < seatingChart[i].length; j++) {
+//         console.log(j, seatingChart[i][j])
+//     }
+// }
+
+
+for(let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`Row #${i + 1}`)
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j])
+    }
 }
