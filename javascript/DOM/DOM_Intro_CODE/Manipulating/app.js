400051ad7d7661e645f0b3e31572822a575bdbee
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-07 06:34:39 -0800

    javascript selectors basics

---
+++ javascript/DOM/DOM_Intro_CODE/Manipulating/app.js
@@ -0,0 +1,16 @@
+// const allImages = document.getElementsByTagName('img');
+
+// for (let image of allImages) {
+//     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// }
+
+// const squareImages = document.getElementsByClassName('square');
+
+// for (let image of squareImages) {
+//     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// }
+
+// const links = document.querySelectorAll('p a');
+// for (let link of links) {
+//     console.log(link.href)
+// }
