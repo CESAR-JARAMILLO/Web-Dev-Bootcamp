8d3f7c1bc95d0c581da440fb38108da500af21d2
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-26 08:38:21 -0800

    express basics

--- express/FirstApp/index.js
+++ express/FirstApp/index.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
     res.send("This is the home page!!!")
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`Browsing the ${subreddit} subreddit`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`Browsing the ${postId} on the ${subreddit} subreddit`)
+})
+
 app.post('/cats', (req, res) => {
     res.send('Post request to /cats!! This is different from a get request!!!')
 })
@@ -25,9 +35,15 @@ app.get('/dogs', (req, res) => {
     res.send('WOOF!!!')
 })
 
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    res.send(`<h1>Search results for: ${q}</h1>`)
+})
+
 app.get('*', (req, res) => {
     res.send(`I don't know that path!`)
 })
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!' )
