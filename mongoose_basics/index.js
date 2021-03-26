351fc9d3e2cc257da7e78a81c2f302f1a74bde97
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-03-26 10:11:00 -0700

    first mongoose model

--- mongoose_basics/index.js
+++ mongoose_basics/index.js
@@ -7,3 +7,13 @@ mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, u
         console.log('Oh no error!!!')
         console.log(err)
     })
+
+    const movieSchema = new mongoose.Schema({
+        title: String,
+        year: Number,
+        score: Number,
+        rating: String
+    });
+
+    const Movie = mongoose.model('Movie', movieSchema);
+    const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'});
