2fe22f4de00be080d8fa11f0dbf47c1139e1a2a9
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-04-01 11:59:06 -0700

    campground model basics

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -3,7 +3,17 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 // connects to mongo database
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
 
 const app = express();
 
@@ -12,6 +22,10 @@ app.set('views', path.join(__dirname, 'views'))
 
 
 app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('makecampground', (req, res) => {
     res.render('home')
 })
 
